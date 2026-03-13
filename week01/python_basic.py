cba10f2922e31bcaaa9299646ba880b853104387
houngnawe <houngnawe2000@gmail.com>
2026-03-13 15:48:45 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -292,5 +292,7 @@ number = 0
 for score in [90, 25, 67, 45, 93]:
     number += 1
     if score > 60: 
-     print("%d번 학생은 합격입니다." %)
+     print("%d번 학생은 합격입니다." % number)
+    else:
+     print("%d")
 
