a1baf6f34392644e598b76f1da42bc0dbbede05e
houngnawe <houngnawe2000@gmail.com>
2026-03-22 20:44:04 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -64,12 +64,11 @@ print(more2015_tv.head())
 more2015_or_tv = netflix[(netflix['release_year'] > 2015) | (netflix['type'] == 'TV Show')]
 print(more2015_or_tv.head())
 
-data = {
-    'name': ['Alice', 'Bob, 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Hannah' ],
+data = {'name': ['Alice', 'Bob, 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Hannah' ],
     'comment_lenght': [150, 200, 50, 300, 120, 180, 75, 160],
     'likes': [25. 30, 10, 45, 20, 35, 5, 28],
     'is_spam': [False, False, True, False, False, True, False, False],
     'has_image': [True, False, True, True, False, False, True, True]
 }
 df = pd.DataFrame(data)
-print(df.head()
+print(df.head())
