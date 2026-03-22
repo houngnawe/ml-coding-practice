a516ba5f64d8213d67a01f3df5c5db1de7f094f6
houngnawe <houngnawe2000@gmail.com>
2026-03-22 21:08:34 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -87,4 +87,6 @@ print(winner_df)
 
 print(sample_df.reset_index())
 
-print(sample_df.reset_index(drop=True))    # 기존 인덱스는 제거
+print(sample_df.reset_index(drop=True))    # 기존 인덱스는 제거하기
+
+print(sample_df.set_)
