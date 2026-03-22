6e8c30fc12e30a4d5d08484a5978f1a70ceeaa7e
houngnawe <houngnawe2000@gmail.com>
2026-03-22 20:31:04 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -52,4 +52,5 @@ print(netflix['release_year'] > 2015)
 more2015 = netflix[netflix['release_year'] > 2015]
 print(more2015.head(10))
 
-print(~())
+print(~(netflix['release_year'] > 2015))
+less2015 = 
