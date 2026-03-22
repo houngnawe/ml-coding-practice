3bc1c844a21477f5dd2cba371bd60a266ea9ba47
houngnawe <houngnawe2000@gmail.com>
2026-03-22 23:07:35 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+netflix = pd.read_csv('')
+
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample.csv'
 sample = pd.read_csv(file_url)
 
