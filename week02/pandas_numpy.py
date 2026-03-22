1042010f0f8224a1472450d6bbf2ffc456b22c94
houngnawe <houngnawe2000@gmail.com>
2026-03-22 20:35:04 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -59,4 +59,6 @@ print(less2015.head())
 print((netflix['release_year'] > 2015) & (netflix['type'] == 'TV Show'))
 
 more2015_tv = netflix[(netflix['release_year'] > 2015) & (netflix['type'] == 'TV Show')]
-print(more2015_tv)
+print(more2015_tv.head())
+
+more2015_or_tv = 
