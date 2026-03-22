572128de621e2916ed368ec5f943c1015291ef67
houngnawe <houngnawe2000@gmail.com>
2026-03-22 20:49:34 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -76,7 +76,7 @@ print(df.head())
 
 # 필터링 조건 설정
 condition = (
-    (df['comment_lenght'] >= 100) &
+    (df['comment_lenght'] >= 100) &   #댓글 길이 100자
     (df['likes'] >= 20) &
     (~df['is_spam']) & 
     (df['has_image'])
