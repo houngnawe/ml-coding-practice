8ba996fa5ea89dab408121887005b2c5738a1fd4
houngnawe <houngnawe2000@gmail.com>
2026-03-22 20:17:04 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -30,4 +30,5 @@ print(sample_df[['var_1', 'var_4']])  # [ ][ ]를 사용하면 [] 가 하나의 
 
 # loc 는 location의 앞글자
 print(sample_df.loc['a'])             # 행 기준으로 인덱싱
-print(sample_df)
+print(sample_df.loc[['a','c','e']])
+
