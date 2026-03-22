4490bb447a18bcb6562698b828b786366c7a40d4
houngnawe <houngnawe2000@gmail.com>
2026-03-22 21:33:35 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -112,5 +112,6 @@ print(iris['class'].value_counts())
 
 # 예제 데이터 생성
 data = {
-    'name': ['Alice', 'Bob', 'Charle']
+    'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+    'age' : [25, ]
 }
