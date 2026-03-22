9070c300405268611cea945d5fa7a962fed5e0b6
houngnawe <houngnawe2000@gmail.com>
2026-03-22 20:23:04 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -39,5 +39,5 @@ print(sample_df.iloc[0:2])
 print(sample_df.iloc[0:3])
 print(sample_df.iloc[0:3, 2:4])      # 컬럼까자 동시에 인덱싱
 
-print(sample_df.drop(['var_1','var-3']))
+print(sample_df.drop(['var_1','var_3'], axis=1))    # 
 
