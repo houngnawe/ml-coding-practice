3fe282ff924256e5db6a9d51ef9643ee5c3bf9e0
houngnawe <houngnawe2000@gmail.com>
2026-03-22 21:29:35 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -103,4 +103,6 @@ print(iris.groupby('class').std())
 
 print(iris.drop('class', axis=1).agg(['sum','mean', 'std']))
 
-print(iris.agg(['sum','mean', 'std']))      # calss 컬럼으로 인해
+# print(iris.agg(['sum','mean', 'std']))      # calss 컬럼으로 인해 에러 발생
+
+
