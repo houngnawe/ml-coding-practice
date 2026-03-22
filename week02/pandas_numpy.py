78a239a5724be9308cd9ec52f37b8295e77addc7
houngnawe <houngnawe2000@gmail.com>
2026-03-22 22:45:05 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -202,5 +202,7 @@ print(np.random.choice(['red','green','white','black','blue'],size=3, replace=Fa
 
 print(np.arange(1,11))
 print(np.arange(1,11,2))
-print(np.linspace(1,100,10))      # 1부터 10까지 균등한 간겨
+print(np.linspace(1,100,10))      # 1부터 10까지 균등한 간격의 값을 4개 추출
+
+A = np.a
  
