6b64cff2d58618e8ccba9b377887221c95027fcb
houngnawe <houngnawe2000@gmail.com>
2026-04-17 16:27:13 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -25,5 +25,7 @@ def main():
     start = jsonResponse['start'] + jsonResponse['display']
     jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
     
-    print('전체 검색 : %d ')
+    print('전체 검색 : %d 건' %total)
+    
+    with open('%s ')
         
