cf1d81d9e8634b4b29505c2b013d8b09b7c3d967
houngnawe <houngnawe2000@gmail.com>
2026-04-17 16:23:43 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -20,4 +20,6 @@ def main():
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
          for post in jsonResponse['time']:
              cnt += 1
-             getPostData(post, jsonResult, cnt)           # 
+             getPostData(post, jsonResult, cnt)           # [CODE 3]
+             
+        start = 
