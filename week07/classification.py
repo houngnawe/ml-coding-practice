a2aa644aadbaf8621db2291267450e66babb9292
houngnawe <houngnawe2000@gmail.com>
2026-05-03 20:00:57 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -17,4 +17,8 @@ def plot_digit(image_data):
     image = image_data.reshape(28, 28)
     plt.imshow(image, camp="binary")
     plt.axis("off")
-     
+    
+some_digit = X[0]
+plot_digit(some_digit)
+plt.show
+) 
