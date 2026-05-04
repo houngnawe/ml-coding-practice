c38087ab979d19d875aa9160a1e2da32c2e599d1
houngnawe <houngnawe2000@gmail.com>
2026-05-04 19:34:33 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -99,7 +99,6 @@ forest_clf = RandomForestClassifier(random_state=42)
 
 y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
                                     method="decision_proba")
-roc_auc_score(y_train_5, y_scores)
 
 y_scores_forest = y_probas_forest[:, 1]
 y_train_pred_forest = y_probas_forest[:, 1]>= 0.5  # 양성 확률 ≥ 50%
