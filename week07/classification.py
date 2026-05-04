ad1e1b13088d433c32d8af014cfaf47aca88917c
houngnawe <houngnawe2000@gmail.com>
2026-05-04 19:33:33 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -88,7 +88,7 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import roc_auc_score
 
 y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
-                             method="decision_function")
+                             method="decision_probs")
 
 roc_auc_score(y_train_5, y_scores)
 
