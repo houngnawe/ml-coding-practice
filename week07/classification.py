3a61c296d2553c08c303b8116556ac0b242ecbd2
houngnawe <houngnawe2000@gmail.com>
2026-05-04 19:18:03 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -94,4 +94,4 @@ from sklearn.ensemble import RandomForestClassifier
 
 forest_clf = RandomForestClassifier(random_state=42)
 
-y_probas_
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_)
