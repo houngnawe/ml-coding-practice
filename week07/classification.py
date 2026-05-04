b2fdd4ebfb76b5c0bd165bfc8922219690ad3982
houngnawe <houngnawe2000@gmail.com>
2026-05-04 18:56:03 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -58,15 +58,15 @@ print(cross_val_score(dummy_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
 #오차 행렬
 from sklearn.model_selection import cross_val_predict
 
-y_train_prep = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 
 from sklearn.model_selection import cross_val_predict
 
-y_train_prep = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 
 from sklearn.metrics import confusion_matrix
 
-cm = confusion_matrix(y_train_5, y_train_prep)
+cm = confusion_matrix(y_train_5, y_train_pred)
 print(cm)
 
 y_train_perfect_predictions = y_train_5 # 완벽한 분류기알 경우
@@ -75,4 +75,4 @@ print(confusion_matrix(y_train_5, y_train_perfect_predictions))
 # 정밀도와 재현율
 from sklearn.metrics import precision_score, recall_score
 
-print(precision_score(y_train_5, y_))
+print(precision_score(y_train_5, y_train_pred))
