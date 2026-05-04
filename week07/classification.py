b16684636e1f09953e8d3d43da49018788ca6de6
houngnawe <houngnawe2000@gmail.com>
2026-05-04 18:49:33 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -62,4 +62,6 @@ y_train_prep = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 
 from sklearn.model_selection import cross_val_predict
 
-y_train_prep = cross_val_predict(sgd_clf, X_train, y_)
+y_train_prep = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+
+from sklearn.metrics import 
