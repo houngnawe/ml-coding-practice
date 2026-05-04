d4d98e3aff57f9d6a95663fc9e107d3f207efa8e
houngnawe <houngnawe2000@gmail.com>
2026-05-04 19:27:33 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 
 def plot_digit(image_data):
     image = image_data.reshape(28, 28)
-    plt.imshow(image, camp="binary")
+    plt.imshow(image, cmap="binary")
     plt.axis("off")
     
 some_digit = X[0]
