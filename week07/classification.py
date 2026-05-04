ff9f388c34cb1cb26ab840ff6da47874d169dc07
houngnawe <houngnawe2000@gmail.com>
2026-05-04 19:17:03 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -89,4 +89,7 @@ y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
 
 roc_auc_score(y_train_5, y_score)
 
-# 다음은 실행하는데 몇 분이 걸릴 수 있
+# 다음은 실행하는데 몇 분이 걸릴 수 있음
+from sklearn.ensemble import RandomForestClassifier
+
+forest
