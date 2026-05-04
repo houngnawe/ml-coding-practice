f0fcdaf36568a54216341bad339ed65f43c5ed18
houngnawe <houngnawe2000@gmail.com>
2026-05-04 18:54:03 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -73,4 +73,5 @@ y_train_perfect_predictions = y_train_5 # 완벽한 분류기알 경우
 print(confusion_matrix(y_train_5, y_train_perfect_predictions))
 
 # 정밀도와 재현율
-from sklearn.metrics import
+from sklearn.metrics import precision_score, recall_score
+
