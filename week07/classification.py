96c103c9dc1643c2b1aa15b516e3497b733f47f0
houngnawe <houngnawe2000@gmail.com>
2026-05-04 19:32:03 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -47,7 +47,9 @@ sgd_clf.fit(X_train,y_train_5)
 sgd_clf.predict([some_digit])
 
 # 성능 측정 - 교자 검증을 사용한 정확도 측정
-from sklearn.
+from sklearn.model_selection import cross_val_score
+
+crp\
 from sklearn.dummy import DummyClassifier
 
 dummy_clf = DummyClassifier()
